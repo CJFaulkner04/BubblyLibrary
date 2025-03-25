f126032b8473de7bab4888a13301c7cbdbfe4741
CarlosJFaulkner <cfaulk12@uncc.edu>
2025-03-25 18:55:34 -0400

    done

--- Nav/BubblyNav/index.js
+++ Nav/BubblyNav/index.js
@@ -11,6 +11,7 @@ window.onscroll = () => {
         nav.style.borderRadius= "20px";
         nav.style.padding = "5px";
         nav.style.boxShadow = " 0 0px 5px 1px rgba(0,0,0,0.1)"
+        nav.style.marginTop = "10px";
         ul.style.width = "200px";
         //acc.innerHTML = `<i class="fa-solid fa-phone fa-md"></i>`;
         acc.style.width ="30px";
@@ -20,13 +21,14 @@ window.onscroll = () => {
                     <img src="images/me.png">
                 </div>`;
     } else {
-        nav.style.backgroundColor = "white";
+        //nav.style.backgroundColor = "white";
         nav.style.width = "100vw";
         nav.style.border = "none";
         nav.style.borderBottom = "1px solid rgb(223, 223, 223)";
         nav.style.borderRadius = "0";
         nav.style.padding = "10px 0";
-        nav.style.boxShadow = "none"
+        nav.style.boxShadow = "none";
+        nav.style.marginTop = "0";
         ul.style.width = "250px";
        // acc.innerHTML = `<i class="fa-solid fa-phone fa-md"></i> <i class="fa-brands fa-linkedin-in fa-lg"></i> <i class="fa-brands fa-github-alt fa-lg"></i>`;
         acc.style.width ="100px";
